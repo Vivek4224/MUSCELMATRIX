e7ee961fa12428765a4d95cea9ed20e5bc44955f
Vivek4224 <vivekbarvaliya4224@gmail.com>
2024-10-15 19:30:45 +0530

    design

--- MMApps/web/urls.py
+++ MMApps/web/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from MMApps.web.views import webViewObject
+from MMApps.web.views import webViewObject,authViewObject
 
 urlpatterns = [
     path('', webViewObject.index_view, name='index_view'),
+    path('login/', authViewObject.login_view, name='login_view'),
+    path('register/', authViewObject.register_view, name='register_view'),
+
 ]
--- MMApps/web/views.py
+++ MMApps/web/views.py
@@ -4,4 +4,12 @@ class webView:
     def index_view(self, request):
         return render(request, 'web/index.html')
     
+class authView:    
+    def login_view(self, request):
+        return render(request, 'web/login.html')
+    
+    def register_view(self, request):
+        return render(request, 'web/signUp.html')
+    
 webViewObject = webView()
+authViewObject = authView()

8aa9a720f56cc206f958a82d207593f8d0f72040
Vivek4224 <vivekbarvaliya4224@gmail.com>
2025-01-26 21:28:26 +0530

    add services page

--- MMApps/web/urls.py
+++ MMApps/web/urls.py
@@ -3,6 +3,7 @@ from MMApps.web.views import webViewObject,authViewObject
 
 urlpatterns = [
     path('', webViewObject.index_view, name='index_view'),
+    path('services/', webViewObject.services_view, name='services_view'),
     path('about/', webViewObject.about_view, name='about_view'),
     path('contact/', webViewObject.contact_view, name='contact_view'),
     path('profile/', webViewObject.profile_view, name='profile_view'),
--- MMApps/web/views.py
+++ MMApps/web/views.py
@@ -32,6 +32,8 @@ def login_required(view_func):
 class webView:
     def index_view(self, request):
         return render(request, 'web/index.html')
+    def services_view(self, request):
+        return render(request, 'web/services.html')
     
     def about_view(self, request):
         return render(request, 'web/About.html')
